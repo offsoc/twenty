6125c7ae92f05a9800743b2cd1ae575888935bc5
martmull <martmull@hotmail.fr>
2025-04-30 16:47:56 +0200

    Add relations in database event trigger output data

--- packages/twenty-server/src/modules/workflow/workflow-builder/workflow-schema/utils/generate-fake-object-record.ts
+++ packages/twenty-server/src/modules/workflow/workflow-builder/workflow-schema/utils/generate-fake-object-record.ts
@@ -1,3 +1,5 @@
+import { FieldMetadataType } from 'twenty-shared/types';
+
 import { ObjectMetadataEntity } from 'src/engine/metadata-modules/object-metadata/object-metadata.entity';
 import {
   Leaf,
@@ -9,13 +11,27 @@ import { shouldGenerateFieldFakeValue } from 'src/modules/workflow/workflow-buil
 
 const generateObjectRecordFields = (
   objectMetadataEntity: ObjectMetadataEntity,
-) =>
-  objectMetadataEntity.fields.reduce(
+  depth = 0,
+) => {
+  return objectMetadataEntity.fields.reduce(
     (acc: Record<string, Leaf | Node>, field) => {
       if (!shouldGenerateFieldFakeValue(field)) {
         return acc;
       }
 
+      if (field.type === FieldMetadataType.RELATION && depth === 0) {
+        acc[field.name] = {
+          isLeaf: false,
+          icon: field.icon,
+          label: field.label,
+          value: generateObjectRecordFields(
+            field.relationTargetObjectMetadata,
+            1,
+          ),
+        };
+
+        return acc;
+      }
       acc[field.name] = generateFakeField({
         type: field.type,
         label: field.label,
@@ -26,6 +42,7 @@ const generateObjectRecordFields = (
     },
     {},
   );
+};
 
 export const generateFakeObjectRecord = (
   objectMetadataEntity: ObjectMetadataEntity,
